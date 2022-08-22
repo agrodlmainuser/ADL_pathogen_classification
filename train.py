0bfa2cb4bfae49eddaed5be6221673cb19f85eba
agrodl_main <111681601+agrodlmainuser@users.noreply.github.com>
2022-08-22 18:01:07 +0300

    Added train class
    for initial training, still need to add a retrain class or method

--- train.py
+++ train.py
@@ -17,6 +17,7 @@ import xml.etree.ElementTree as ET
 script_parameters = ADL_Read_XML("AgroDL_Classification_TL_Training_0001")
 input_dir = script_parameters.get_params("original_input_dir")
 val_dir = script_parameters.get_params("original_val_dir")
+classification_model_save_root = script_parameters.get_params("classification_model_save_root")
 
 
 # hyper parameters value setting
@@ -34,7 +35,8 @@ for class_dir in os.listdir(train_dir):
     for i,image in enumerate(os.listdir(f'{train_dir}/{class_dir}')):
       if i%5 == 0:
         shutil.move(f'{train_dir}/{class_dir}/{image}', f'{class_val_dir}/{image}')
-        
+
+
 train_ds = tf.keras.preprocessing.image_dataset_from_directory(
     train_dir,
     batch_size=BATCH_SIZE)
@@ -45,65 +47,85 @@ val_ds = tf.keras.preprocessing.image_dataset_from_directory(
 class_names = train_ds.class_names
 n_classes = len(class_names)
 
-# TL classification models
-mobilenet_v3 = "https://tfhub.dev/google/imagenet/mobilenet_v3_large_100_224/feature_vector/5"
-efficientnet_v2 = "https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet21k_b2/feature_vector/2"
-inception_v3 = "https://tfhub.dev/google/tf2-preview/inception_v3/feature_vector/4"
 
-# This function takes a model and its required image size and performs training on the dataset. The output of the function is the model itself.
-def transfer_model(name,IMAGE_SIZE, tl_model_str):
-  resize_and_rescale = tf.keras.Sequential([
-  layers.experimental.preprocessing.Resizing(IMAGE_SIZE, IMAGE_SIZE),
-  layers.experimental.preprocessing.Rescaling(1./255),
-  ])
-  data_augmentation = tf.keras.Sequential([
-  layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
-  layers.experimental.preprocessing.RandomRotation(0.2),
-  ])
-  pretrained_model_without_top_layer = hub.KerasLayer(name, input_shape=(IMAGE_SIZE, IMAGE_SIZE, 3),
-                                                      trainable=False)
-  model = tf.keras.Sequential([resize_and_rescale, data_augmentation, pretrained_model_without_top_layer,
+class Ptgn_Classification:
+  
+  def __init__(self, BATCH_SIZE=32, CHANNELS=3, EPOCHS=15, train_dir, val_dir, classification_model_save_root, n_classes):
+    self.BATCH_SIZE = BATCH_SIZE
+    self.CHANNELS = CHANNELS
+    self.EPOCHS = EPOCHS
+    self.train_dir = train_dir
+    self.val_dir = val_dir
+    self.classification_model_save_root = classification_model_save_root
+    self.n_classes = n_classes
+
+    
+  def prepare_ds(self):
+    train_ds = tf.keras.preprocessing.image_dataset_from_directory(self.train_dir, batch_size=self.BATCH_SIZE)
+    val_ds = tf.keras.preprocessing.image_dataset_from_directory(self.val_dir,batch_size=self.BATCH_SIZE)
+    return train_ds, val_ds
+  
+  def train(self, tl_model_name_str):
+    self.tl_model_name_str = tl_model_name_str
+    # pulling the tl models from tf hub
+    if self.tl_model_name_str == "mobilenet_v3":
+      tl_model = "https://tfhub.dev/google/imagenet/mobilenet_v3_large_100_224/feature_vector/5"
+    elif self.tl_model_name_str == "efficientnet_v2":
+      tl_model = "https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet21k_b2/feature_vector/2"
+    elif self.tl_model_name_str == "inception_v3":
+      tl_model = "https://tfhub.dev/google/tf2-preview/inception_v3/feature_vector/4"
+    # activating previous function to get the ds at the right struction
+    train_ds, val_ds = self.prepare_ds()
+    resize_and_rescale = tf.keras.Sequential([layers.experimental.preprocessing.Resizing(self.IMAGE_SIZE, self.IMAGE_SIZE),
+                                              layers.experimental.preprocessing.Rescaling(1./255),])
+    data_augmentation = tf.keras.Sequential([layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
+                                             layers.experimental.preprocessing.RandomRotation(0.2),])
+    pretrained_model_without_top_layer = hub.KerasLayer(tl_model, input_shape=(self.IMAGE_SIZE, self.IMAGE_SIZE, 3), trainable=False)
+    
+    model = tf.keras.Sequential([resize_and_rescale, data_augmentation, pretrained_model_without_top_layer,
                                tf.keras.layers.Dense(256, activation='relu'),
                                tf.keras.layers.Dense(n_classes, activation='softmax')])
-  model.build([None, IMAGE_SIZE, IMAGE_SIZE, 3]) 
-  model.summary()
-  model.compile(optimizer="adam", loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
-                metrics=['accuracy'])
-  history = model.fit(train_ds, batch_size=BATCH_SIZE,
-                       validation_data=val_ds, verbose=1,
-                      epochs=EPOCHS,)
-  acc = history.history['accuracy']
-  val_acc = history.history['val_accuracy']
-  loss = history.history['loss']
-  val_loss = history.history['val_loss']
-  # here a new output directory is created for the saved model
-  classification_model_save_root = "/content/drive/MyDrive/Colab Notebooks/AgroML_Test_System/Clasification_Models/"
-  for i in range(1000):
-    if os.path.isdir(f"{classification_model_save_root}/exp{i}_{tl_model_str}_blob"):
-      continue
-    else:
-      os.mkdir(f"{classification_model_save_root}/exp{i}_{tl_model_str}_blob")
-      break
-  model.save(f"{classification_model_save_root}/exp{i}_{tl_model_str}_blob")
-  plt.figure(figsize=(8, 8))
-  plt.subplot(1, 2, 1)
-  plt.plot(range(EPOCHS), acc, label='Training Accuracy')
-  plt.plot(range(EPOCHS), val_acc, label='Validation Accuracy')
-  plt.legend(loc='lower right')
-  plt.title('Training and Validation Accuracy')
+    model.build([None, self.IMAGE_SIZE, self.IMAGE_SIZE, 3]) 
+    model.summary()
+		model.compile(optimizer="adam", loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False), metrics=['accuracy'])
+    history = model.fit(train_ds, batch_size=self.BATCH_SIZE, validation_data=val_ds, verbose=1,epochs=self.EPOCHS,)
+    # for visualing the model
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    # draw a graph
+    plt.figure(figsize=(8, 8))
+    plt.subplot(1, 2, 1)
+    plt.plot(range(self.EPOCHS), acc, label='Training Accuracy')
+    plt.plot(range(self.EPOCHS), val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
 
-  plt.subplot(1, 2, 2)
-  plt.plot(range(EPOCHS), loss, label='Training Loss')
-  plt.plot(range(EPOCHS), val_loss, label='Validation Loss')
-  plt.legend(loc='upper right')
-  plt.title('Training and Validation Loss')
-  plt.show()
-  plt.figure(figsize=(20, 50))
-  
-  return model
+    plt.subplot(1, 2, 2)
+    plt.plot(range(self.EPOCHS), loss, label='Training Loss')
+    plt.plot(range(self.EPOCHS), val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.show()
+    plt.figure(figsize=(20, 50))
 
-# model execution 
-our_model = transfer_model(mobilenet_v3, 224, "mobilenet_v3")
+  def model_save(self, blob=False):
+    # here a new output directory is created for the saved model
+    for i in range(1000):
+      if blob:
+        extn = f"exp{i}_{self.tl_model_name_str}_blob"
+      else:
+        extn = f"exp{i}_{self.tl_model_name_str}"
+      if os.path.isdir(f"{self.classification_model_save_root}/{extn}"):
+        continue
+      else:
+        os.mkdir(f"{self.classification_model_save_root}/{extn}")
+        break
+      model.save(f"{self.classification_model_save_root}/{extn}")
+
+
+
 
 
 # Retrain an existed model
